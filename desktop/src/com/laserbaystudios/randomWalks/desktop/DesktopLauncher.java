a2c9839ff40675788584754930e2380e5d0185ce
Greg <gregory.albing@jacks.sdstate.edu>
2014-05-11 13:30:43 -0500

    initial committed version

---
+++ desktop/src/com/laserbaystudios/randomWalks/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.laserbaystudios.randomWalks.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.laserbaystudios.randomWalks.randomWalks;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new randomWalks(), config);
+	}
+}
